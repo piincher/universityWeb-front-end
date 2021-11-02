55bef4b43c7d678ad87cfe8fd9e183473b3b04f2
Ibrahim Kouma <69568495+piincher@users.noreply.github.com>
2021-11-02 23:22:17 +0800

    prepare for deployement

--- src/pages/HomePage.component.jsx
+++ src/pages/HomePage.component.jsx
@@ -2,13 +2,14 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import CustomButton from '../components/CustomButton.component';
 import UniversitiesList from '../components/UniversitiesList.component';
+import Search from '../components/Search.component';
 
 export const HomePage = ({ history }) => {
 	const [ universities, setUniversities ] = useState([]);
 
 	useEffect(() => {
 		const fetchAll = async () => {
-			const { data } = await axios.get('http://localhost:8000/api');
+			const { data } = await axios.get('https://universityback-end.herokuapp.com/');
 			setUniversities(data);
 		};
 		fetchAll();
@@ -20,6 +21,8 @@ export const HomePage = ({ history }) => {
 
 	return (
 		<React.Fragment>
+			<Search />
+			<h1>All avalaible universities</h1>
 			<UniversitiesList universities={universities} />
 			<CustomButton handleSubmit={handleSubmit}>Add new University</CustomButton>
 		</React.Fragment>
